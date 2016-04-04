99fe7389a1c173e4bb227c4a78b2e8f9383a9829
azallae <azallae2@gmail.com>
2016-04-04 09:14:28 -0700

    Update GameServerTCP.java

--- Chicken Game/src/game/GameServerTCP.java
+++ Chicken Game/src/game/GameServerTCP.java
@@ -14,11 +14,11 @@ public class GameServerTCP extends GameConnectionServer<UUID>
 	
 	public void acceptClient(IClientInfo ci, Object o){
 		String message = (String)o;
-		String[] messageTokens = message.split(",");
+		String[] msgTokens = message.split(",");
 		
-		if(messageTokens.length > 0){
-			if(messageTokens[0].compareTo("join")==0){
-				UUID clientID = UUID.fromString(messageTokens[1]);
+		if(msgTokens.length > 0){
+			if(msgTokens[0].compareTo("join")==0){
+				UUID clientID = UUID.fromString(msgTokens[1]);
 				addClient(ci, clientID);
 				sendJoinedMessage(clientID, true);
 			}
@@ -39,7 +39,7 @@ public class GameServerTCP extends GameConnectionServer<UUID>
 		}
 		
 		if(msgTokens[0].compareTo("create") == 0){
-			UUID clientID = UUID.fromString(messageTokens[1]);
+			UUID clientID = UUID.fromString(msgTokens[1]);
 			String[] pos = {msgTokens[2], msgTokens[3], msgTokens[4]};
 			sendCreateMessages(clientID, pos);
 			sendWantsDetailsMessages(clientID);
@@ -52,7 +52,7 @@ public class GameServerTCP extends GameConnectionServer<UUID>
 			sendDetailsMessages(clientID, remoteID, pos);
 		}
 		
-		if(msgTokens[0].compareTo("move")){
+		if(msgTokens[0].compareTo("move")==0){
 			UUID clientID = UUID.fromString(msgToken[1]);
 			String[] pos = {msgTokens[3], msgTokens[4], msgTokens[5]};
 			sendMoveMessages(clientID, pos);
@@ -80,6 +80,17 @@ public class GameServerTCP extends GameConnectionServer<UUID>
 		catch(IOException e){ e.printStackTrace(); }
 	}
 	
+	public void sendMoveMessages(UUID clientID, String[] position){
+		try{
+			String message = new String("move, " + clientID.toString());
+			message += ", " + position[0];
+			message += ", " + position[1];
+			message += ", " + position[2];
+			forwardPacketToAll(message, clientID);
+		}
+		catch(IOException e){ e.printStackTrace(); }
+	}
+	
 	public void sendDetailsMessages(UUID clientID, UUID remoteID, String[] position){
 		try{
 			String message = new String("dsfr, " + clientID.toString());
@@ -91,13 +102,10 @@ public class GameServerTCP extends GameConnectionServer<UUID>
 		catch(IOException e){ e.printStackTrace(); }
 	}
 	
-	public void sendWantsDetailsMessages(UUID clientID, UUID remoteID, String[] position){
+	public void sendWantsDetailsMessages(UUID clientID){
 		try{
-			String message = new String("dsfr, " + clientID.toString());
-			message += ", " + position[0];
-			message += ", " + position[1];
-			message += ", " + position[2];
-			sendPacket(message, remoteID);
+			String message = new String("wsds, " + clientID.toString());
+			forwardPacketToAll(message, clientID);
 		}
 		catch(IOException e){ e.printStackTrace(); }
 	}
