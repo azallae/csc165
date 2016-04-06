46c0c52178ac4dee6906c988b19b346349fdf986
azallae <azallae2@gmail.com>
2016-04-05 21:14:29 -0700

    Update Starter.java

--- Chicken Game/src/gameStarter/Starter.java
+++ Chicken Game/src/gameStarter/Starter.java
@@ -1,10 +1,28 @@
 package gameStarter;
 
-import game.ChickenGame;
+import game.*;
 
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Starter {
+	private static final int localPort = 6000;
+	
 	public static void main (String [] args){
 		new ChickenGame().start(); 
+		Scanner kb = new Scanner(System.in);
+		String ans = "";
+		System.out.print("Are you the host?");
+		ans = kb.nextLine();
+		if(ans.charAt(0) == 'y'){
+			System.out.println("You are now hosting the server.");
+			GameServerTCP testTCPServer = new GameServerTCP(localPort);
+			System.out.println("Your localInetAddress: " + testTCPServer.getLocalInetAddress);
+			System.out.println("Port number is: " + localPort);
 		}
+		else{
+			System.out.print("You are a client.");
+			
+		}
+	}
 }
