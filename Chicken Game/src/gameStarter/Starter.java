d1038e8f241e57c367996fae71709d5899b183d9
azallae <azallae2@gmail.com>
2016-04-05 21:58:10 -0700

    Update Starter.java

--- Chicken Game/src/gameStarter/Starter.java
+++ Chicken Game/src/gameStarter/Starter.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Starter {
-	private static final int localPort = 6000;
+	private static final int localPort = 1550;
 	
 	public static void main (String [] args){
 		Scanner kb = new Scanner(System.in);
@@ -16,7 +16,7 @@ public class Starter {
 		if(ans.charAt(0) == 'y'){
 			System.out.println("You are now hosting the server.");
 			GameServerTCP testTCPServer = new GameServerTCP(localPort);
-			System.out.println("Your localInetAddress: " + testTCPServer.getLocalInetAddress);
+			System.out.println("Your localInetAddress: " + testTCPServer.getLocalInetAddress());
 			System.out.println("Port number is: " + localPort);
 			
 			String[] msgTokens = testTCPServer.getLocalInetAddress().toString().split("/");
@@ -25,7 +25,7 @@ public class Starter {
 		}
 		else{
 			System.out.print("You are a client.");
-			MyNetworkingGame testTCPClient = new MyNetworkingGame("130.86.96.232", localPort);
+			MyNetworkingGame testTCPClient = new MyNetworkingGame("130.86.96.235", localPort);
 			testTCPClient.start();
 		}
 	}
